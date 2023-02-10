2043c4cc83b6167a8d1c11664bb622c92223da60
motchy <motchy869@gmail.com>
2023-02-10 15:08:17 +0900

    refactor: use reg as output

--- projects/01_blink_LED_1_with_SystemVerilog/clock_divider.sv
+++ projects/01_blink_LED_1_with_SystemVerilog/clock_divider.sv
@@ -6,22 +6,19 @@ module clock_divider #(
 (
     input wire clk_in,
     input wire rst,
-    output wire clk_out
+    output reg clk_out = 1'b0
 );
 
-reg clk_out_i = 1'b0;
-reg [$bits(RATE)-1:0] count = 0;
+logic [$bits(RATE)-1:0] count = 0;
 
-assign clk_out = clk_out_i;
-
-always @(clk_in, rst) begin
+always_ff @(posedge clk_in, posedge rst) begin
     if (rst) begin
         count <= 0;
-        clk_out_i <= 1'b0;
+        clk_out <= 1'b0;
     end else begin
         if (count == RATE-1) begin
             count <= 0;
-            clk_out_i <= ~clk_out_i;
+            clk_out <= ~clk_out;
         end else begin
             count <= count + 1;
         end
